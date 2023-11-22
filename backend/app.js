86b99ca319149f50610eb65cd430a3b390e460be
victoria <victorian114@gmail.com>
2023-11-22 13:52:18 -0800

    done with phase3

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const { environment } = require('../config');
+const { environment } = require('./config');
 const isProduction = environment === 'production';
 const { ValidationError } = require('sequelize');
 
