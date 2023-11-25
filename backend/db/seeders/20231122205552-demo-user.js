672a86ad35e127a55476496dce8b3be23b9116df
victoria <victorian114@gmail.com>
2023-11-25 12:45:16 -0800

    added seeds

--- backend/db/seeders/20231122205552-demo-user.js
+++ backend/db/seeders/20231122205552-demo-user.js
@@ -13,17 +13,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstname: 'abcdefg',
+        lastname: 'qwerty'
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstname: 'poiuyt',
+        lastname: 'zxcvbn'
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstname: 'asdfgh',
+        lastname: 'ghjklm'
       }
     ], { validate: true });
   },
